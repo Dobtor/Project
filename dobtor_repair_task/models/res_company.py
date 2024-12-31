adcb8d21d2b956aff220768386b2bece2fdb296f
Ken-Dobtor <ken.lin@dobtor.com>
2024-12-31 12:42:46 +0800

    [ADD&FIX]dobtor_repair_task: 修正派工階段、新增維修單返修product設定
    - 派工階段現為：
        - 已承攬
        - 已寄回
        - 完工
        - 取消

--- dobtor_repair_task/models/res_company.py
+++ dobtor_repair_task/models/res_company.py
@@ -12,8 +12,11 @@ class ResCompany(models.Model):
             ('service_tracking', '=', 'task_global_project'),
         ]
     
-    on_site_done_stage = fields.Many2one('project.task.type', string='On Site Done Stage')
-    factory_return_stage = fields.Many2one('project.task.type', string='Factory Return Stage')
+    cancel_stage = fields.Many2one('project.task.type', string='Cancel Stage')
+    done_stage = fields.Many2one('project.task.type', string='Done Stage')
+    process_stage = fields.Many2one('project.task.type', string='Process Stage')
+    rma_done_stage = fields.Many2one('project.task.type', string='Return To Manufacturer Done Stage')
     installation_task_product = fields.Many2one('product.product', string='Installation Task Product', domain=_get_task_product_domain)
     repair_task_product = fields.Many2one('product.product', string='Repair Task Product', domain=_get_task_product_domain)
+    repair_return_task_product = fields.Many2one('product.product', string='Repair(Return) Task Product', domain=_get_task_product_domain)
     return_task_product = fields.Many2one('product.product', string='Return Task Product', domain=_get_task_product_domain)
--- dobtor_repair_task/models/res_config_setting.py
+++ dobtor_repair_task/models/res_config_setting.py
@@ -5,8 +5,11 @@ from odoo import models, fields, api, _
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
     
-    on_site_done_stage = fields.Many2one('project.task.type', related='company_id.on_site_done_stage', string='On Site Done Stage', readonly=False)
-    factory_return_stage = fields.Many2one('project.task.type', related='company_id.factory_return_stage', string='Factory Return Stage', readonly=False)
+    cancel_stage = fields.Many2one('project.task.type', related='company_id.cancel_stage', string='Cancel Stage', readonly=False)
+    done_stage = fields.Many2one('project.task.type', related='company_id.done_stage', string='Done Stage', readonly=False)
+    process_stage = fields.Many2one('project.task.type', related='company_id.process_stage', string='Process Stage', readonly=False)
+    rma_done_stage = fields.Many2one('project.task.type', related='company_id.rma_done_stage', string='Return To Manufacturer Done Stage', readonly=False)
     installation_task_product = fields.Many2one('product.product', related='company_id.installation_task_product', string='Installation Task Product', readonly=False)
     repair_task_product = fields.Many2one('product.product', related='company_id.repair_task_product', string='Repair Task Product', readonly=False)
+    repair_return_task_product = fields.Many2one('product.product', related='company_id.repair_return_task_product', string='Repair(Return) Task Product', readonly=False)
     return_task_product = fields.Many2one('product.product', related='company_id.return_task_product', string='Return Task Product', readonly=False)
