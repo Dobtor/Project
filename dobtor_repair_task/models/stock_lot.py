a2dc486be8ff44d5bf092262bbdfcf4abc3cc233
Ken-Dobtor <ken.lin@dobtor.com>
2025-01-09 23:25:22 +0800

    [FIX]dobtor_repair_task: 修正維修repair建立派工單
    - context：task_create，用於判斷是否建立派工單
    - 經銷商為repair partner的主用戶預設經銷商

--- dobtor_repair_task/models/stock_lot.py
+++ dobtor_repair_task/models/stock_lot.py
@@ -7,19 +7,22 @@ class StockLot(models.Model):
     
     def create_repair_order(self):
         repair_order = super().create_repair_order()
-        partner = repair_order.partner_id
-        repair_task_product = repair_order.company_id.repair_task_product
-        SaleOrderSudo = self.env['sale.order'].sudo()
-        repair_task_order = SaleOrderSudo.create({
-            'partner_id': partner.commercial_partner_id.id,
-            'partner_shipping_id': partner.id,
-            'order_line': [(0, 0, {
-                'product_uom_qty': 1,
-                'product_uom': repair_task_product.uom_id.id,
-                'product_id': repair_task_product.id,
-                'distributor_ids': [(6, 0, [partner.distributor.user_ids[:1].id] if partner.distributor else [])],
-            })],
-        })
-        repair_task_order.action_confirm()
-        repair_order.return_task_id = repair_task_order.tasks_ids[:1].id
+        
+        if repair_order and self.env.context.get('task_create'):
+            partner = repair_order.partner_id.commercial_partner_id
+            repair_task_product = repair_order.company_id.repair_task_product
+            SaleOrderSudo = self.env['sale.order'].sudo()
+            repair_task_order = SaleOrderSudo.create({
+                'partner_id': partner.id,
+                'partner_shipping_id': repair_order.partner_id.id,
+                'order_line': [(0, 0, {
+                    'product_uom_qty': 1,
+                    'product_uom': repair_task_product.uom_id.id,
+                    'product_id': repair_task_product.id,
+                    'distributor_ids': [(6, 0, [partner.distributor.user_ids[:1].id] if partner.distributor else [])],
+                })],
+            })
+            repair_task_order.action_confirm()
+            repair_order.return_task_id = repair_task_order.tasks_ids[:1].id
+
         return repair_order
